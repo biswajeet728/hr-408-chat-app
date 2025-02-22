b6be17011da8e0d84aed11b972834edddd601267
biswajeet <biswajeet@carstream.co>
2025-02-23 02:04:49 +0530

    index file updated !

--- service/src/index.js
+++ service/src/index.js
@@ -45,10 +45,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messagesRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
